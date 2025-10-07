41a847af37364161ae0bb2cb0f506281f1f03a05
Stephen Kelly <gilgalad195@gmail.com>
2025-10-07 14:16:50 -0500

    added simple fileserver

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	h := http.FileServer(http.Dir("."))
+	mux.Handle("/", h)
+
 	s := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
+
 	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatalf("encountered an error: %v", err)
